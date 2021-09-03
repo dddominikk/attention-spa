d8c612f3072255bf1bb8b81a679131f335f9b4ff
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2021-09-03 18:26:27 +0200

    Create footer.svelte

---
+++ src/footer.svelte
@@ -0,0 +1,21 @@
+<script>
+const 
+	currentYear = new Date(Date.now()).getFullYear(),
+	footerContents = `©️2020-${currentYear}, Attention Spa, vl. Dominik Bošnjak. All rights reserved.`
+</script>
+
+<footer>
+	{footerContents}
+</footer>
+
+
+<style>
+	footer {
+		position:relative;
+		height: 5%;
+		margin-top: 300;
+		font-weight:1;
+		font-family: 'Work Sans', sans-serif;
+	}
+
+</style>
