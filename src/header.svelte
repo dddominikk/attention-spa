6a001e27ba59f608d0e3a3f86d6fb3053d3b2bcd
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2021-09-03 18:25:33 +0200

    Create header.svelte

---
+++ src/header.svelte
@@ -0,0 +1,21 @@
+<header>
+	<h1 align="center" class="header">Attention Spa</h1>
+</header>
+
+<style>
+	header{
+		    display: block;
+				color: coral;
+    		margin: auto;
+				height: 10%;
+    		width: 100%;
+	}
+	
+	h1 {
+		position:absolute;
+		font-weight:1;
+		font-family: 'Work Sans', sans-serif;
+
+		position:relative;
+	}
+</style>
